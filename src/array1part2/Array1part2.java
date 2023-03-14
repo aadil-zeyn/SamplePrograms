b3e9ef10c4ae64e4db8a55fabe54de2cb49be9dc
Aadil <aadil1857aa@gmail.com>
2023-03-14 23:10:21 +0530

    modifies array program

--- src/array1part2/Array1part2.java
+++ src/array1part2/Array1part2.java
@@ -4,8 +4,11 @@ public class Array1part2 {
 
 	public static void main(String[] args) {
 		int n=5;
-		int[] array=new int[5];
-		array={3,2,5,5,4};
+		int[] array=new int[]{3,2,5,5,4};
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		
 
 	}
 
